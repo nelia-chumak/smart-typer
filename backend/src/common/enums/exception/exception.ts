046d7feb10469621d5d0405d5b802a8d95144432
nelia-chumak <nelia.chumak.dev@gmail.com>
2026-01-03 22:11:05 +0100

    Removed unnecessary .js on back route

--- backend/src/common/enums/exception/exception.ts
+++ backend/src/common/enums/exception/exception.ts
@@ -2,5 +2,5 @@ export {
   HttpErrorMessage,
   ValidationErrorMessage,
 } from 'smart-typer-shared/common/enums/enums';
-export * from './multer-error-message/multer-error-message.enum.js';
+export * from './multer-error-message/multer-error-message.enum';
 export * from './socket-error-message/socket-error-message.enum';
